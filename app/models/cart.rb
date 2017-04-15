565be2303bdfc72b89a78b00404144a1eeb6e038
Sean Clarke <xseanclarke@gmail.com>
2017-04-15 18:00:39 +0000

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,8 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
 
+
+  def add_item(item_id)
+  end
 end
--- db/migrate/20140805184430_create_line_items.rb
+++ db/migrate/20140805184430_create_line_items.rb
@@ -1,5 +1,9 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
-
+    create_table :lineitems do |t|
+      t.integer :item_id
+      t.integer :cart_id
+      t.integer :quantity, default: 1
+    end
   end
 end
